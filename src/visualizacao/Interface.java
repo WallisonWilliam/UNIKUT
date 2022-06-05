5563c5078e93c636e1b173ff32d83bef5e6d8895
Wallison William <91322466+WallisonWilliam@users.noreply.github.com>
2022-06-05 11:38:06 -0300

    Update Interface.java

--- src/visualizacao/Interface.java
+++ src/visualizacao/Interface.java
@@ -13,7 +13,7 @@ public class Interface {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		Scanner input = new Scanner(System.in);
-		char opCont, opPerf;
+		char opPerf, opCont;
 		boolean validCont;
 		String email, senha;
 		Conta cont;
@@ -30,50 +30,61 @@ public class Interface {
 			System.out.println("Digite 0 para fechar a pagina da rede social." + ANSI_RESET);
 			System.out.print(ANSI_YELLOW + "Digite aqui: " + ANSI_RESET);
 			opCont = in.next().charAt(0);
-			if (opCont == '1' || opCont == '2') {
-				System.out.print(ANSI_YELLOW + "Digite as informa�oes necessarias para a verificacao de conta ");
+			switch (opCont) {
+			case '1':
+				System.out.print(ANSI_YELLOW + "Digite as informaçoes necessarias para a verificacao de conta ");
 				System.out.println("(Voce nao podera trocar o email depois de ter logado!).");
 				System.out.print("Email: ");
 				email = input.nextLine();
 				System.out.print("Senha: " + ANSI_RESET);
 				senha = input.nextLine();
 				cont = new Conta(email, senha);
-				if (opCont == '1') {
-					menuCadastrar(cont, listCont);
+				menuCadastrar(cont, listCont);
+				break;
+			case '2':
+				System.out.print(ANSI_YELLOW + "Digite as informaçoes necessarias para a verificacao de conta ");
+				System.out.println("(Voce nao podera trocar o email depois de ter logado!).");
+				System.out.print("Email: " + ANSI_RESET);
+				email = input.nextLine();
+				System.out.print("Senha: " + ANSI_RESET);
+				senha = input.nextLine();
+				cont = new Conta(email, senha);
+				validCont = perf.entrarPerf(listCont, cont);
+				if (validCont == true) {
+					menuUnikut();
+					do {
+						System.out.println(ANSI_RESET);
+						System.out.println(ANSI_BLUE + "Menu do perfil." + ANSI_RESET);
+						System.out.println(ANSI_YELLOW + "Digite 5 para ver o menu novamente.");
+						System.out.print("Informe a sua opcao desejada: " + ANSI_RESET);
+						opPerf = in.next().charAt(0);
+						if (opPerf == '1') {
+							listCont.vizlPerf(cont);
+						} else if (opPerf == '2') {
+							perf.alterPerf(listCont, cont);
+						} else if (opPerf == '3') {
+							opAmg.opAmigo(listCont, cont);
+						} else if (opPerf == '4') {
+							opRec.opRecados(listCont, cont);
+						} else if (opPerf == '5') {
+							menuUnikut();
+						} else if (opPerf == '0') {
+							System.out.println(ANSI_WHITE + "Esperamos te ver de novo por aqui." + ANSI_RESET);
+						} else {
+							System.out.println(ANSI_RED + "Opcao invalida.");
+						}
+					} while (opPerf != '0');
+
+				} else if (opCont == '3') {
+					listCont.exibCont();
+				} else if (opCont == '0') {
+					System.out.println(ANSI_WHITE + "Volte sempre aqui." + ANSI_RESET);
 				} else {
-					validCont = perf.entrarPerf(listCont, cont);
-					if (validCont == true) {
-						menuUnikut();
-						do {
-							System.out.println(ANSI_RESET);
-							System.out.println(ANSI_BLUE + "Menu do perfil." + ANSI_RESET);
-							System.out.println(ANSI_YELLOW + "Digite 5 para ver o menu novamente.");
-							System.out.print("Informe a sua opcao desejada: " + ANSI_RESET);
-							opPerf = in.next().charAt(0);
-							if (opPerf == '1') {
-								listCont.vizlPerf(cont);
-							} else if (opPerf == '2') {
-								perf.alterPerf(listCont, cont);
-							} else if (opPerf == '3') {
-								opAmg.opAmigo(listCont, cont);
-							} else if (opPerf == '4') {
-								opRec.opRecados(listCont, cont);
-							} else if (opPerf == '5') {
-								menuUnikut();
-							} else if (opPerf == '0') {
-								System.out.println(ANSI_WHITE + "Esperamos te ver de novo por aqui." + ANSI_RESET);
-							} else {
-								System.out.println(ANSI_RED + "Opcao invalida.");
-							}
-						} while (opPerf != '0');
-					}
+					System.out.println(ANSI_RED + "Opcao invalida." + ANSI_RESET);
 				}
-			} else if (opCont == '3') {
-				listCont.exibCont();
-			} else if (opCont == '0') {
-				System.out.println(ANSI_WHITE + "Volte sempre aqui." + ANSI_RESET);
-			} else {
-				System.out.println(ANSI_RED + "Opcao invalida." + ANSI_RESET);
+				break;
+			default:
+				System.out.println(ANSI_RED + "Opção invalida" + ANSI_RESET);
 			}
 		} while (opCont != '0');
 	}
