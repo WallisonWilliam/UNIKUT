e7d6c27001acf3e57280b0d426dd67e139beaf12
Wallison William <91322466+WallisonWilliam@users.noreply.github.com>
2022-06-05 12:56:26 -0300

    Add files via upload

---
+++ src/model/Conta.java
@@ -0,0 +1,111 @@
+package model;
+
+import static view.Cores.*;
+
+import control.ListaUnikut;
+
+public class Conta {
+	private String email;
+	private String senha;
+	private String nome;
+	private int idade = 0;
+	private String genero = "Sem informacao";
+	private int dia = 0;
+	private int mes = 0;
+	private int ano = 0;
+	private String estadCiv = "Sem informacao";
+	private String descric = "Sem informacao";
+	private String statAmiz;
+	public String recadAmiz = "Sem mensagens enviadas";
+	private final ListaUnikut listaAmigo = new ListaUnikut();
+
+	public Conta(String email, String senha) {
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public Conta(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public String getSenha() {
+		return this.senha;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public String getStatAmiz() {
+		return this.statAmiz;
+	}
+
+	public String getRecadAmiz() {
+		return this.recadAmiz;
+	}
+
+	public ListaUnikut getListaAmigo() {
+		return this.listaAmigo;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public void setEstadCiv(String estadCiv) {
+		this.estadCiv = estadCiv;
+	}
+
+	public void setDescric(String descric) {
+		this.descric = descric;
+	}
+
+	public void setStatAmiz(String statAmiz) {
+		this.statAmiz = statAmiz;
+	}
+
+	public void setRecadAmiz(String recadAmiz) {
+		this.recadAmiz = recadAmiz;
+	}
+
+	public int compareTo(Conta bcCont) {
+		int compara;
+		compara = this.email.compareToIgnoreCase(bcCont.getEmail());
+		return compara;
+	}
+
+	public String toString() {
+		return ANSI_GREEN + "Email: " + this.email + "\n" + "Nome: " + this.nome + "\n" + "Genero: " + this.genero
+				+ "\n" + "Idade: " + this.idade + "\n" + "Data de aniversario: " + this.dia + "/" + this.mes + "/"
+				+ this.ano + "\n" + "Estado civil: " + this.estadCiv + "\n" + "Descricao do perfil: " + this.descric
+				+ ANSI_RESET;
+	}
+}
---
+++ src/view/Interface.java
@@ -0,0 +1,88 @@
+package view;
+
+import java.util.Scanner;
+
+import control.ListaUnikut;
+import model.Conta;
+
+import static view.Cores.*;
+import static view.Menu.*;
+
+public class Interface {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
+		char opPerf, opCont;
+		boolean validCont;
+		String email, senha;
+		Conta cont;
+		ListaUnikut listCont = new ListaUnikut();
+		Perfil perf = new Perfil();
+		AmigoUnikut opAmg = new AmigoUnikut();
+		RecadoUnikut opRec = new RecadoUnikut();
+		System.out.println();
+		System.out.println(ANSI_WHITE + "Bem-vindo a rede social Unikut." + ANSI_RESET);
+		do {
+			System.out.println();
+			System.out.println(ANSI_BLUE + "Deseja cadastrar uma conta? Se sim, digite 1.");
+			System.out.println("Se voce ja e cadastrado aqui, digite 2 para logar.");
+			System.out.println("Digite 0 para fechar a pagina da rede social." + ANSI_RESET);
+			System.out.print(ANSI_YELLOW + "Digite aqui: " + ANSI_RESET);
+			opCont = in.next().charAt(0);
+			switch (opCont) {
+			case '1':
+				System.out.print(ANSI_YELLOW + "Digite as informaçoes necessarias para a verificacao de conta ");
+				System.out.println("(Voce nao podera trocar o email depois de ter logado!).");
+				System.out.print("Email: " + ANSI_RESET);
+				email = input.nextLine();
+				System.out.print(ANSI_YELLOW + "Senha: " + ANSI_RESET);
+				senha = input.nextLine();
+				cont = new Conta(email, senha);
+				menuCadastrar(cont, listCont);
+				break;
+			case '2':
+				System.out.print(ANSI_YELLOW + "Digite as informaçoes necessarias para a verificacao de conta ");
+				System.out.println("(Voce nao podera trocar o email depois de ter logado!).");
+				System.out.print("Email: " + ANSI_RESET);
+				email = input.nextLine();
+				System.out.print(ANSI_YELLOW + "Senha: " + ANSI_RESET);
+				senha = input.nextLine();
+				cont = new Conta(email, senha);
+				validCont = perf.entrarPerf(listCont, cont);
+				if (validCont == true) {
+					menuUnikut();
+					do {
+						System.out.println(ANSI_RESET);
+						System.out.println(ANSI_BLUE + "Menu do perfil." + ANSI_RESET);
+						System.out.println(ANSI_YELLOW + "Digite 5 para ver o menu novamente.");
+						System.out.print("Informe a sua opcao desejada: " + ANSI_RESET);
+						opPerf = in.next().charAt(0);
+						if (opPerf == '1') {
+							listCont.vizlPerf(cont);
+						} else if (opPerf == '2') {
+							perf.alterPerf(listCont, cont);
+						} else if (opPerf == '3') {
+							opAmg.opAmigo(listCont, cont);
+						} else if (opPerf == '4') {
+							opRec.opRecados(listCont, cont);
+						} else if (opPerf == '5') {
+							menuUnikut();
+						} else if (opPerf == '0') {
+							System.out.println(ANSI_WHITE + "Esperamos te ver de novo por aqui." + ANSI_RESET);
+						} else {
+							System.out.println(ANSI_RED + "Opcao invalida.");
+						}
+					} while (opPerf != '0');
+				}
+				break;
+			case '0':
+				System.out.println(ANSI_WHITE + "Encerrando o programa, volte sempre!." + ANSI_RESET);
+				break;
+			default:
+				System.out.println(ANSI_RED + "Opçao invalida" + ANSI_RESET);
+			}
+		} while (opCont != '0');
+
+	}
+
+}
